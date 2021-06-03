196bd2fe5e2d4a8d0fd6f6f8359fb76d21b7bec2
xXxbaodeptraixXx <85320210+xXxbaodeptraixXx@users.noreply.github.com>
2021-06-04 06:16:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,6 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. Yêu Quỳnh Anh nhiều nhiều 😘😘',
     messLink: 'http://fb.com//thachgiabao210' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
